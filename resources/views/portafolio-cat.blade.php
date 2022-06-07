4ae9bae131770bd6b3e99eaea39f8ac29ebf320e
Alejandro Proenza <dev@laraceramica.com>
2022-06-07 16:04:17 +0300

    CPT list taxonomy

---
+++ resources/views/portafolio-cat.blade.php
@@ -0,0 +1,22 @@
+{{--
+  Template Name: Portfolio categories
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php(the_post())
+    @include('partials.page-header')
+    @include('partials.content-page')
+    
+    @php( $art_cat = get_categories( ['taxonomy' => 'portafolio_cat'] ) )
+      @foreach( $art_cat as $a )
+         <a href=
+         {!! get_category_link( $a->term_id ) !!}
+            
+            > {!! $a->name !!} </a>, 
+      @endforeach
+
+  @endwhile
+@endsection
+   
